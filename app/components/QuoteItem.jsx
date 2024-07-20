5d1389069ef650d845211623c90c750bfee8407a
WizardOfSaaS <WizardOfSaaS@proton.me>
2024-07-20 08:51:39 +0530

    Fix quote escape issue

--- app/components/QuoteItem.jsx
+++ app/components/QuoteItem.jsx
@@ -2,7 +2,7 @@ function QuoteItem({ quote }) {
   const { content, author } = quote;
   return (
     <div>
-      <div id="text">"{content}"</div>
+      <div id="text">{`"${content}"`}</div>
       <div id="author">-- {author}</div>
     </div>
   );
